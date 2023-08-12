bf71f4676edc76d1cfd8fa137b434eb10e3a5c04
n00bCod3r <n00bCod3r@protonmail.com>
2023-08-12 02:20:04 +0200

    new filter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import albums from 'src/app/data/albums.json';
+
+export interface Album {
+  artist: string
+  title: string
+  displaytitle?: string
+  country: string
+  year: string
+  genres: string
+  url: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,9 +18,17 @@ import albums from 'src/app/data/albums.json';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  albumArray: any[] = JSON.parse(JSON.stringify(albums));
+  albumArray: Album[] = JSON.parse(JSON.stringify(albums));
+  filteredArray: Album[] = JSON.parse(JSON.stringify(this.albumArray));
+  searchTerm = new FormControl<string>('');
 
-  goToLink(url: string) {
+  filterAlbums(): void {
+    const st: string = this.searchTerm.value!;
+    this.filteredArray = this.albumArray.filter(album => album.artist.toLowerCase().includes(st) || album.title.toLowerCase().includes(st) || album.displaytitle?.toLowerCase().includes(st) || album.country.toLowerCase().includes(st) || album.genres.toLowerCase().includes(st) || album.year.includes(st));
+    console.log(this.filteredArray);
+  }
+
+  goToLink(url: string): void {
     window.open(url, "_blank");
   }
 }
