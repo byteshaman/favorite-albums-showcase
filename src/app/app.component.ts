70f076415b0c8d516c46cde7d5077fea27964858
n00bCod3r <n00bCod3r@protonmail.com>
2023-11-08 01:20:25 +0100

    improve filter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import albums from 'src/app/data/albums.json';
 
-export interface Album {
-  artist: string
-  title: string
-  displaytitle?: string
-  displayartist?: string
-  extra?: string
-  country: string
-  year: string
-  genres: string
-  url: string
+interface Album {
+  artist: string;
+  title: string;
+  displaytitle?: string;
+  displayartist?: string;
+  extra?: string;
+  country: string;
+  year: string;
+  genres: string;
+  url: string;
 }
+
+interface Checkbox {
+  value: string;
+  label: string;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -23,14 +29,63 @@ export class AppComponent {
   albumArray: Album[] = JSON.parse(JSON.stringify(albums));
   filteredArray: Album[] = JSON.parse(JSON.stringify(this.albumArray));
   searchTerm = new FormControl<string>('');
+  formGroup: FormGroup;
+  checkboxes: Checkbox[];
+
+  constructor(private formBuilder: FormBuilder)  {
+    this.formGroup = this.formBuilder.group({
+      artist: [true],
+      title: [true],
+      country: [true],
+      year: [true],
+      genres: [true]
+    });
+
+    this.checkboxes = [
+      {value: 'artist', label: 'Artist'},
+      {value: 'title', label: 'Title'},
+      {value: 'country', label: 'Country'},
+      {value: 'year', label: 'Year'},
+      {value: 'genres', label: 'Genre'}
+    ]
+  }
+
+  allCheckboxesDisabled(): boolean {
+    // this.formGroup.value = key/value pair for every control of the form group
+    return Object.entries(this.formGroup.value).every(([key, value]) => value === false);
+  }
 
   filterAlbums(): void {
     const st: string = this.searchTerm.value!;
-    this.filteredArray = this.albumArray.filter(album => album.artist.toLowerCase().includes(st) || album.displayartist?.toLowerCase().includes(st) || album.title.toLowerCase().includes(st) || album.displaytitle?.toLowerCase().includes(st) || album.country.toLowerCase().includes(st) || album.genres.toLowerCase().includes(st) || album.year.includes(st));
-    console.log(this.filteredArray);
+    this.filteredArray = this.albumArray
+      .filter(album => false // pointless, just to align stuff
+        || ((album.artist.toLowerCase().includes(st) || album.displayartist?.toLowerCase().includes(st)) && this.formGroup.get('artist')?.value) 
+        || ((album.title.toLowerCase().includes(st) || album.displaytitle?.toLowerCase().includes(st)) && this.formGroup.get('album')?.value)
+        || (album.country.toLowerCase().includes(st) && this.formGroup.get('country')?.value)
+        || (album.genres.toLowerCase().includes(st) && this.formGroup.get('genres')?.value)
+        || (album.year.toLowerCase().includes(st) && this.formGroup.get('year')?.value)
+      );
+    // console.log(this.filteredArray);
   }
 
   goToLink(url: string): void {
     window.open(url, "_blank");
+  }
+
+  onCheckboxChange(): void {
+    if (this.allCheckboxesDisabled()) {
+      this.restoreCheckboxes();
+    }
+    this.filterAlbums();
+  }
+
+  restoreCheckboxes(): void {
+    this.formGroup.patchValue({
+      artist: true,
+      title: true,
+      country: true,
+      year: true,
+      genres: true
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
